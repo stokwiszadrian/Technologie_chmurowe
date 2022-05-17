ca94bcf0f54a98ab4ca75bb4073df6c8aae82937
Adrian Stokwisz <stokwiszadrian@gmail.com>
2022-05-17 12:19:40 +0200

    zad 2, 3

--- Lab9/zad1/src/App.js
+++ Lab9/zad1/src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don't Learn React
         </a>
+        <code>react_project:release1</code>
       </header>
     </div>
   );
